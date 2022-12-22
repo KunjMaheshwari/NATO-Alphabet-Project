cbfde90cdb8f87bf66101e44dcec143ffa99b375
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2022-12-22 23:27:24 +0530

    Added the concept of Error Handling in Python

--- NATO ALPHABET/main.py
+++ NATO ALPHABET/main.py
@@ -2,10 +2,17 @@ import pandas
 
 data = pandas.read_csv("nato_alphabet.csv")
 
-# Creating a dictionary from a csv file
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
 
-ask_user = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in ask_user]
-print(output_list)
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+is_reading = True
+while is_reading:
+  ask_user = input("Enter a word: ").upper()
+  try:
+    output_list = [phonetic_dict[letter] for letter in ask_user]
+  except KeyError:
+    print("Sorry only Alphabets are allowed")
+    is_reading = True
+  else:
+    print(output_list)
+    is_reading = False
